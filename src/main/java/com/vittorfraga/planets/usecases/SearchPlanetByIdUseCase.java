86f34b2b11790a7e02f5d62e462364c12a7b9dc0
Carlos Fraga <vittorfraga@outlook.com>
2023-12-13 15:23:28 -0300

    refatorado caso de uso de listar pelo id

--- src/main/java/com/vittorfraga/planets/usecases/SearchPlanetByIdUseCase.java
+++ src/main/java/com/vittorfraga/planets/usecases/SearchPlanetByIdUseCase.java
@@ -5,8 +5,6 @@ import com.vittorfraga.planets.dtos.PlanetResponse;
 import com.vittorfraga.planets.entities.Planeta;
 import com.vittorfraga.planets.repositories.PlanetaRepository;
 import com.vittorfraga.planets.usecases.exceptions.ResourceNotFoundException;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,14 +16,6 @@ public class SearchPlanetByIdUseCase {
     @Autowired
     private PlanetaRepository planetaRepository;
 
-    @Operation(
-            description = "Buscar planeta pelo ID",
-            summary = "Buscar planeta pelo ID",
-            responses = {
-                    @ApiResponse(description = "ok", responseCode = "200"),
-                    @ApiResponse(description = "Id não encontrado", responseCode = "404"),
-            }
-    )
     public PlanetResponse execute(Long id) {
         Planeta planeta = planetaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Planeta não encontrado com o id: " + id));
         return DtoMapper.toResponse(planeta);
